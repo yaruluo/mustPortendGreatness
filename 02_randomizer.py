3480c41bcf1d5ecebe2bbfbd5336ae5c9f1ae0a6
Yaru Luo <yluo00@stuy.edu>
2019-09-12 00:16:26 -0400

    combined w emory

--- 02_randomizer.py
+++ 02_randomizer.py
@@ -1,22 +1,21 @@
-# Team RaDUCKal
-# Yaru & Kelvin
+# Team Ducking Awesome
+# Emory & Yaru
 
 KREWES = {
-	'orpheus': ['Emily', 'Kevin', 'Vishwaa', 'Eric', 'ray', 'Jesse', 'Tiffany', 'Amanda', 'Junhee', 'Jackie ', 'Tyler', 'Emory', 'Ivan', 'Elizabeth', 'Pratham', 'Shaw', 'Eric', 'Yaru', 'Kelvin', 'Hong Wei', 'Michael', 'Kiran', 'Amanda', 'Joseph', 'Tanzim', 'David', 'Yevgeniy'],
-	'rex': ['William', 'Joseph', 'Calvin', 'Ethan', 'Moody', 'Mo', 'Big Mo', 'Peihua', 'Saad', 'Benjamin', 'Justin', 'Alice', 'Hilary', 'Ayham', 'Michael', 'Matthew', 'Jionghao', 'Devin ', 'David', 'Jacob', 'Will', 'Hannah', 'Alex'],
-	'endymion': ['Grace', 'Nahi', 'Derek', 'Jun Tao', 'Connor', 'Jason', 'Tammy', 'Albert', 'Kazi', 'Derek', 'Brandon', 'Kenneth', 'Lauren', 'Biraj', 'Jeff', 'Jackson', 'Taejoon', 'Kevin', 'Jude', 'Sophie', 'Henry', 'Coby', 'Manfred', 'Leia', 'Ahmed', 'Winston']
+	'orpheus': [ 'Emily', 'Kevin', 'Vishwaa', 'Eric', 'ray', 'Jesse', 'Tiffany', 'Amanda', 'Junhee', 'Jackie ', 'Tyler', 'Emory', 'Ivan', 'Elizabeth', 'Pratham', 'Shaw', 'Eric', 'Yaru', 'Kelvin', 'Hong Wei', 'Michael', 'Kiran', 'Amanda', 'Joseph', 'Tanzim', 'David', 'Yevgeniy'],
+	'rex': [ 'William', 'Joseph', 'Calvin', 'Ethan', 'Moody', 'Mo', 'Big Mo', 'Peihua', 'Saad', 'Benjamin', 'Justin', 'Alice', 'Hilary', 'Ayham', 'Michael', 'Matthew', 'Jionghao', 'Devin ', 'David', 'Jacob', 'Will', 'Hannah', 'Alex'],
+	'endymion': [ 'Grace', 'Nahi', 'Derek', 'Jun Tao', 'Connor', 'Jason', 'Tammy', 'Albert', 'Kazi', 'Derek', 'Brandon', 'Kenneth', 'Lauren', 'Biraj', 'Jeff', 'Jackson', 'Taejoon', 'Kevin', 'Jude', 'Sophie', 'Henry', 'Coby', 'Manfred', 'Leia', 'Ahmed', 'Winston']
 }
 
+#PRECONDITION: team is one of the three team names: Orpheus, Rex or Endymion
+def getName( team):
+    if not( team in KREWES.keys()):
+        return "The chosen team is not an option."
+    return random.choice( KREWES[ team])
 
-def randomStudent( givenList): # givenList is KREWES
-  import random
-  teamNo = random.randrange( 2)
-  if teamNo == 0:
-     print( random.choice( KREWES[ 'orpheus']) + " from Team Orpheus")
-  if teamNo == 1:
-     print( random.choice( KREWES[ 'rex']) + " from Team Rex")
-  if teamNo == 2:
-     print( random.choice( KREWES[ 'endymion']) + " from Team Endymion")
 
-     
-randomStudent( 'rex')
+print( getName( "hdfsaj"))
+print( getName( "orpheus"))
+print( getName( "orpheus"))
+print( getName( "rex"))
+print( getName( "endymion"))
