6455f78f9780124c09f1caa19911c72d04ed11f3
Yaru Luo <yluo00@stuy.edu>
2019-09-16 08:09:18 -0400

    added csv file

--- 03_csv/duckingAwesome_Luo-Y_Walsh-E.py
+++ 03_csv/duckingAwesome_Luo-Y_Walsh-E.py
@@ -19,15 +19,18 @@ for item in rows[1: len( rows) - 1]:
     dictionary[ key] = value
 
 
-
-#returns a randomly selected occupation where the results are weighted by the percentage given
+    
+#returns a randomly selected occupation where the results
+#are weighted by the percentage given
 def randomOccupation():
-    currentNo = 0
-    currentOcc = "" 
+    currentNo = 0 
+    currentOcc = ""
     randomNo = random.randint( 0, 998) #total % is 99.8
     for item in dictionary:
         currentOcc = item
-        currentNo += dictionary.get( item) * 10
+        #parse through the dictionary until you reach
+        #the random number, then return the occ
+        currentNo += dictionary.get( item) * 10  
         if currentNo >= randomNo:
             return currentOcc
     #return currentOcc
