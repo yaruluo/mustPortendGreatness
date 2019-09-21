a6f06fd71aee8a95b7e7c901340b6ed9a4df56d9
Yaru Luo <yluo00@stuy.edu>
2019-09-21 15:55:29 -0400

    added 09_stat/template and static folders

---
+++ 09_stat/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    print( "hello")
+    return "hello world"
+
+coll = [0, 1, 1, 2, 3, 5, 8]
+
+@app.route("/my_foist_template")
+def test_tmplt():
+    print( coll)
+    return render_template('my_foist_template.html',
+                            foo = "foooo",
+                            collection = coll)
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
