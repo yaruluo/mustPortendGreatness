aa067ff8f2656bae6aff14386996630f9aa9c9e5
Yaru Luo <yluo00@stuy.edu>
2019-09-23 08:02:08 -0400

    merged w AZ

--- 10_occ/app.py
+++ 10_occ/app.py
@@ -1,42 +1,66 @@
-# Yaru Luo
+# Amanda Zheng and Yaru Luo (AI-YA)
 # SoftDev1 pd1
 # K#10: Jinja Tuning
 # 2019-09-24
 
 from flask import Flask, render_template
 app = Flask(__name__) # create instance of class Flask
-
 import csv, random
 
+# creates list of occs and %s
 rows = []
-with open('occupations.csv', 'r') as csvFile:
-    csvreader = csv.reader(csvFile)
+with open( 'occupations.csv', 'r') as csvFile:
+    csvreader = csv.reader( csvFile)
     for row in csvreader:
-        rows.append(row)
-
-dictionary = {}
-for item in rows[1: len( rows) - 1]:
-    key = item[0]
-    value = float( item[1])
+        rows.append( row)
+dictionary = {} 
+for item in rows[ 1: len( rows) - 1]:
+    key = item[ 0]
+    value = float( item[ 1])
     dictionary[ key] = value
 
+# creates list of occs & links
+r = []
+with open( 'link.csv', 'r') as cFile:
+    csvread = csv.reader( cFile)
+    for row in csvread:
+        r.append( row)
+encyclopedia={} 
+for item in r[ 0: len( r)]:
+    key = item[ 0]
+    value = item[ 1]
+    encyclopedia[ key] = value
 
+#make into a dictionary
+def rangePercent( l):
+    di = list( l.keys())
+    l[ di[ 1]] = float( l.get( di[ 1]))
+    for k in range( 1, len( l.keys())):
+        l[ di[ k]] = round( float( l.get( di[ k]) + l.get( di[ k-1])), 1)
+    return l
+dictionary = rangePercent(dictionary)
+
+# uses binary search
+def randomPick(l):
+    rand = random.randint(1,1000)
+    rand = rand/10.0
+    di = list(l.keys())
+    counta = 0
+    countb = len(di)-1
+    if rand>99.8:
+        return "unemployed"
+    while l.get( di[ counta]) < rand and l.get( di[ countb]) > rand:
+        counta += 1
+        countb -= 1
+    if  l.get( di[ countb]) == rand:
+         return di[ countb]
+    if l.get( di[ counta]) >= rand:
+        return di[ counta]
+    else:
+        return di[ countb + 1]
     
-#returns a randomly selected occupation where the results
-#are weighted by the percentage given
-def randomOccupation():
-    currentNo = 0 
-    currentOcc = ""
-    randomNo = random.randint( 0, 998) #total % is 99.8
-    for item in dictionary:
-        currentOcc = item
-        #parse through the dictionary until you reach
-        #the random number, then return the occ
-        currentNo += dictionary.get( item) * 10  
-        if currentNo >= randomNo:
-            return currentOcc
-
-        
+coll = dictionary
+col = encyclopedia
 
 @app.route("/") # assign following fxn to run when route requested
 def hello_world():
@@ -45,15 +69,15 @@ def hello_world():
 
 @app.route("/occupyflaskst")
 def init():
-    print("csving") 
+    print("csving")
     return render_template(
-        'werk.html',
+        'work.html',
         coll = dictionary,   #for the tablified occupations list
-        output = randomOccupation() #for the lucky occupation
+        output = randomPick( coll), #for the lucky occupation
+        col = encyclopedia
         )
-
 
 if __name__ == "__main__":
     #autopilot
-    app.debug = True 
+    app.debug = True
     app.run()
