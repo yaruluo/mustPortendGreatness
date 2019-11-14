367cda051fb98ea605c88d820df3b411ea02244f
Yaru Luo <yluo00@stuy.edu>
2019-11-13 19:56:54 -0500

    creds

--- 25_restrio/app.py
+++ 25_restrio/app.py
@@ -62,13 +62,14 @@ def ip_location_request():
 @app.route('/superhero')
 def _request():
     url = 'http://superheroapi.com/api.php/2503373653110667/720'
-    hdr = { # 
+    hdr = { # ivan did this 
         'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
         'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
         'Accept-Encoding': 'none',
         'Accept-Language': 'en-US,en;q=0.8',
-        'Connection': 'keep-alive'}
+        'Connection': 'keep-alive'
+    }
     req = urllib.request.Request( url, headers=hdr)
     data = json.loads( urllib.request.urlopen(req).read())
     return render_template( 'hero.html', to_render=data, title="Hero API")
