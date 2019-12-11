8b6bb06d178da0d62def62ef5a17b435b04e75a0
Yaru Luo <yluo00@stuy.edu>
2019-12-11 17:26:16 -0500

    random student

--- 27_js/funky_town.js
+++ 27_js/funky_town.js
@@ -1,17 +1,38 @@
+/*
+Yaru Luo
+SoftDev1 pd1
+K27 -- Sequential Progression
+2019-12-11    
+*/
+
+var students = ["alnasserA", "caoT", "chenA", "chenH", "chenJ", "dossE", "elahiT", "galakhovI",
+                "gorbachevY", "hallJ", "lamE", "lauE", "leeJ", "leeJo", "leeR", "liK", "linJ",
+                "linM", "luoY", "ngK", "rawatP", "shawJ", "sofatV", "vuksanajK", "walshE",
+                "xiedengD", "zhangE", "zhangM", "zhengA"]
+// factorial
+var fact = function(num){
+	if (num < 2){
+		return 1 
+	} else {
+		return num * fact(num -1)
+	}
+}
+// fibonacci
 var fib = function( num){
     if( num > 2)
 	return fib( num - 2) + fib( num - 1);
     else
 	return 1;
 };
-
+// greatest common denominator
 var gcd = function( a, b){
     if( b == 0){
 	return a;
     }
     return gcd( b, a % b);
 };
-
+// random student
 var randomStudent = function(){
-    
+  return students[Math.floor(Math.random() * students.length)];
+};
 };
