be9c661f5ddaf173bdd9a655f9b9e5018e642627
Yaru Luo <yluo00@stuy.edu>
2020-02-06 05:23:20 -0500

    K04

--- 03_canvas/js/clickdraw.js
+++ 03_canvas/js/clickdraw.js
@@ -1,7 +1,6 @@
 // retrieve node in DOM via ID
-var c = document.getElementById("slate");
-console.log(c);
-console.log( 'hi');
+var c = document.getElementById( "slate");
+console.log( 'one');
 
 // instantiate a CanvaRenderingContext2D object
 var ctx = c.getContext( "2d");
@@ -10,21 +9,38 @@ var ctx = c.getContext( "2d");
 ctx.fillStyle = "#ff0000";
 ctx.fillRect( 50, 50, 100, 200);
 
-// const canvas = document.getElementById('my-house');
-// const ctx = canvas.getContext('2d');
+var clear = document.getElementById( 'clear');
+var toggle = document.getElementById( 'toggle');
+var mode = 'box';
 
-// // Set line width
-// ctx.lineWidth = 10;
+clear.addEventListener( 'click', function( e){
+    e.preventDefault()
+}
+		      )
 
-// // Wall
-// ctx.strokeRect(75, 140, 150, 110);
+toggle.addEventListener( 'click', function( e){
+    if( mode == "box"){
+	mode = "dot";
+    }
+    else{
+	mode = "box";
+    }
+}
+		       )
 
-// // Door
-// ctx.fillRect(130, 190, 40, 60);
+c.addEventListener( 'click', function( e){
+    var xcor = e.offsetX;
+    var ycor = e.offsetY;
+    if( mode=="box"){
+	ctx.fillRect( xcor, ycor, 50, 50);
+    }
+    else{
+	ctx.fillStyle="black";
+	ctx.beginPath();
+	ctx.ellipse( xcor, ycor, 5, 5, (Math.PI / 4), 0, (2 * Math.PI));
+	ctx.stroke();
+	ctx.fill();
+    }
+}
+		  )
 
-// // Roof
-// ctx.moveTo(50, 140);
-// ctx.lineTo(150, 60);
-// ctx.lineTo(250, 140);
-// ctx.closePath();
-// ctx.stroke();
