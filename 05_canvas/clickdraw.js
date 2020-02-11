267fe74689b3b54a3eaf1180cad9b29c50ff2e48
Yaru Luo <yluo00@stuy.edu>
2020-02-11 08:28:25 -0500

    commentin

--- 05_canvas/clickdraw.js
+++ 05_canvas/clickdraw.js
@@ -1,43 +1,63 @@
-// Pratham Rawat and Yaru Luo
-//SoftDev pd1
-// K04 -- I see a red door
-// 2020 - 02 - 05
+// Team PYthon
+// Yaru and Pratham
+// SoftDev1 pd1
+// K04 -- I See a Red Door...
+// 2020-02-05
 
-// Constants for Drawing
-const BOX_SIDE_LENGTH = 50;
-const CIRCLE_RADIUS = 5;
+// retrieve node in DOM via ID
+var c = document.getElementById( "slate");
 
-var mode = "circle"; //Mode for which shape to draw
-var c = document.getElementById('slate'); // Retrieves DOM objext for canvas
-var ctx = c.getContext("2d"); // Creates context element objext for Canvas
+// instantiate a CanvaRenderingContext2D object
+var ctx = c.getContext( "2d");
 
-// Event listener for drawing on canvas
-c.addEventListener("click", function(e) {
-  console.log("clik");
-  var rect = c.getBoundingClientRect();
-  if(mode == "box") {
-    ctx.fillRect(event.clientX - rect.x, e.clientY - rect.y , BOX_SIDE_LENGTH, BOX_SIDE_LENGTH);
-  } else {
-    ctx.arc(e.clientX - rect.x, e.clientY - rect.y, CIRCLE_RADIUS, 0, Math.PI * 2);
-    ctx.closePath();
-    ctx.fill();
-  }
+var clear = document.getElementById( 'clear');
+var toggle = document.getElementById( 'toggle');
+var mode = 'box';
+
+var drawn = false;
+
+// clears screen
+clear.addEventListener( 'click', function( e){
+    // ctx.fillStyle = "#fff";
+    // ctx.fillRect( 0, 0, c.width, c.height);
+    if( !drawn){
+	e.preventDefault();
+    }
+    else{
+	ctx.fillStyle = "#fff";
+	ctx.fillRect( 0, 0, c.width, c.height);
+	drawn = false;
+    }
 });
 
-// Event listener for toggle button
-document.getElementById('toggle').addEventListener("click", function() {
-  console.log("togl");
-  if(mode == "box") {
-    mode = "circle"
-  } else {
-    mode = "box"
-  }
-  document.getElementById('toggle').innerHTML = mode;
+// toggle bw a box and dot
+toggle.addEventListener( 'click', function( e){
+    if( mode == "box"){
+	mode = "dot";
+    }
+    else{
+	mode = "box";}
+    }
+    toggle.innerHTML = mode;
 });
 
-// Event listener for clear button
-document.getElementById('clear').addEventListener("click", function(e) {
-  console.log("clr");
-  // e.preventDefault();
-  ctx.clearRect(00 0, c.height, c.width);
+// draws the box or dot
+c.addEventListener( 'click', function( e){
+    // gets coords of mouse click
+    var xcor = e.offsetX;
+    var ycor = e.offsetY;
+    if( mode=="box"){
+	drawn = true;
+	ctx.fillRect( xcor, ycor, 50, 50);
+    }
+    else{
+	drawn = true;
+	ctx.fillStyle="black";
+	ctx.beginPath(); // begins or resets path
+	ctx.ellipse( xcor, ycor, 5, 5, ( Math.PI / 4), 0, ( 2 * Math.PI));
+	// omg i cannot math
+	ctx.stroke();
+	ctx.fill();
+    }
 });
+
---
+++ 06_canvas/dotconn.js
@@ -0,0 +1,45 @@
+// Team PYthon
+// Yaru and Pratham
+// SoftDev1 pd1
+// K #06: Dot Dot Dot
+// 2020-02-11
+
+
+// Constants for Drawing
+const CIRCLE_RADIUS = 5;
+
+var c = document.getElementById('playground'); // Retrieves DOM objext for canvas
+var ctx = c.getContext("2d"); // Creates context element objext for Canvas
+var firstClick = true;
+var oldX = null;
+var oldY = null;
+
+// Event listener for drawing on canvas
+c.addEventListener("click", function(e) {
+    console.log("clik");
+    var rect = c.getBoundingClientRect();
+    ctx.arc(e.clientX - rect.x, e.clientY - rect.y, CIRCLE_RADIUS, 0, Math.PI * 2);
+    ctx.closePath();
+
+    ctx.fill();
+
+    if(firstClick) {
+	firstClick = false;
+    } else {
+	ctx.beginPath();
+	ctx.moveTo(e.clientX - rect.x, e.clientY - rect.y);  // last specified point
+	ctx.lineTo(oldX, oldY); // creates new point line to the point from the last specified point
+	ctx.stroke();
+	ctx.closePath();
+    }
+    oldX = e.clientX - rect.x;
+    oldY = e.clientY - rect.y;
+});
+
+// Event listener for clear button
+document.getElementById('clear').addEventListener("click", function(e) {
+    firstClick = true;
+    console.log("clr");
+    // e.preventDefault();
+    ctx.clearRect(0, 0, c.height, c.width);
+});
