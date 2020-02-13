fdb062643f613c72199eeb271278da9ce68cf5a0
Yaru Luo <yluo00@stuy.edu>
2020-02-13 12:21:21 -0500

    finished woo!

--- 08_canvas-movie/dotconn.js
+++ 08_canvas-movie/dotconn.js
@@ -5,27 +5,38 @@
 
 var c = document.getElementById( "slate");
 var ctx = c.getContext( "2d");
+
 // sets things for an Animaniac
 var animID = 0;
 var progress = 0;
 var shrink = false;
+
 // sets things for the movie to start
 var logo = new Image();
 logo.src='img/logo.png';
 var tvID = 0;
-var slope = -1;
+var xSlope = 1;
+var ySlope = 1;
+var xcor = Math.floor( Math.random() * 520);
+var ycor = Math.floor( Math.random() * 550);
 
 document.getElementById( 'movie').addEventListener( 'click', () => {
-    if( tvID == 0)
-	tvID = window.requestAnimationFrame( bounce);
+    window.cancelAnimationFrame( animID);
+    animID = 0;
+    if( tvID == 0) tvID = window.requestAnimationFrame( bounce);
+    xcor = Math.floor( Math.random() * 520); // resets to random location every time clicked
+    ycor = Math.floor( Math.random() * 550);
+    xSlope = 1; // resets slope to default
+    ySlope = 1;
 })
 
 document.getElementById( "start").addEventListener( "click", () => {
-    if( animID == 0)
-	animID = window.requestAnimationFrame( step);
+    window.cancelAnimationFrame( tvID);
+    tvID = 0;
+    if( animID == 0) animID = window.requestAnimationFrame( step);
 })
 
-document.getElementById("stop").addEventListener("click", () => {
+document.getElementById( "stop").addEventListener( "click", () => {
     window.cancelAnimationFrame( animID);
     window.cancelAnimationFrame( tvID);
     animID = 0;
@@ -34,33 +45,37 @@ document.getElementById("stop").addEventListener("click", () => {
 
 function bounce(){
     ctx.clearRect( 0, 0, c.width, c.height);
+    if ( xcor >= 520 || xcor <= 0) xSlope = -xSlope;
+    if ( ycor >= 550 || ycor <= 0) ySlope = -ySlope;
+    xcor = xcor + xSlope; // moves the logo
+    ycor = ycor + ySlope; // moving
     ctx.drawImage( logo
-		   , Math.floor( Math.random() * 520) // xcor
-		   , Math.floor( Math.random() * 550) // ycor
+		   , xcor
+		   , ycor
 		   , 80 // length
 		   , 50 // width
-		 )
-    
+		 );
     if( tvID != 0){
-	window.requestAnimationFrame( bounce);
+	     window.requestAnimationFrame( bounce);
     }
 }
 
 function step(){
-    if(!shrink){ //expanding
+    if( !shrink){ //expanding
 	progress += 1;
-	if(progress >= c.width/2) shrink = true; //if the radius exceeds the canvas screen
-    }else{ //shrinking
+	if( progress >= c.width/2) shrink = true; //if the radius exceeds the canvas screen
+    }
+    else{ //shrinking
 	progress -= 1;
-	if(progress <= 0){ //if the radius goes below zero
+	if( progress <= 0){ //if the radius goes below zero
 	    shrink = false;
 	    progress = 0; //starting the cycle over again
 	}
     }
     ctx.beginPath();
-    ctx.clearRect(0, 0, c.width, c.height); //clearing the screen for the shrinking
+    ctx.clearRect( 0, 0, c.width, c.height); //clearing the screen for the shrinking
     //filling the circle
-    ctx.arc(c.width/2, c.height/2, progress, 0, 2 * Math.PI);
+    ctx.arc( c.width/2, c.height/2, progress, 0, 2 * Math.PI);
     ctx.fillStyle = "lightblue";
     ctx.fill();
     ctx.stroke();
