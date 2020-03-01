af3774dc8a57f9e8011c94650a26e8c6a4aad132
Yaru <yluo00@stuy.edu>
2020-03-01 14:12:15 +0000

    started work

---
+++ 10_mongo/app.py
@@ -0,0 +1,27 @@
+# Team  __ : Yaru && Ivan
+# SoftDev2 pd1
+# K#10: Import/Export Bank
+# 2020-02-29
+
+from bson.json_util import loads
+from pymongo import MongoClient
+
+client = MongoClient()
+db = client.history
+
+db.catalog.drop() 
+catalog = db.catalog
+with open( 'history.json', 'r') as file:
+  content = file.readlines()
+  for line in content:
+    catalog.insert_one( loads( line))
+
+def findEvent( date):
+  '''All events in a specified date.'''
+  results = catalog.find( { 'date': date})
+  print( 'Results Found: {}'.format( results.count()))
+  print()
+  for x in results:
+    print( x[ 'name'])
+
+findEvent( '2012/12/31')
