d4b79d85363d39c487955f77b0178a39ccd933cf
Yaru Luo <yluo00@stuy.edu>
2020-03-03 16:42:36 -0500

    omg please work

--- 10_mongo/app.py
+++ 10_mongo/app.py
@@ -1,4 +1,4 @@
-# Team  __ : Yaru && Ivan
+# Team Yarn : Yaru && Ivan
 # SoftDev2 pd1
 # K#10: Import/Export Bank
 # 2020-02-29
@@ -13,22 +13,32 @@ from bson.json_util import loads
 from pymongo import MongoClient
 
 client = MongoClient()
-db = client.history
-
-db.catalog.drop() 
-catalog = db.catalog
-with open( 'history.json', 'r') as file:
+db = client.test
+yarn = db.yarn
+if( yarn.count()==0):
+  file.open( 'history.json', 'r')
   content = file.readlines()
   for line in content:
-    catalog.insert_one( loads( line))
+    yarn.insert_one( loads( line))
+  for item in db.find({}):
+    print( item)
 
-def findEvent( date):
-  '''On this date...'''
-  results = catalog.find( { 'date': date})
-  print( 'Results Found: {}'.format( results.count()))
-  print()
-  for x in results:
-    print( x[ 'name'])
 
-findEvent( '2012/12/31')
-findEvent( '-279')
+
+# db.catalog.drop() 
+# catalog = db.catalog
+# with open( 'history.json', 'r') as file:
+#   content = file.readlines()
+#   for line in content:
+#     catalog.insert_one( loads( line))
+
+# def findEvent( date):
+#   '''On this date...'''
+#   results = catalog.find( { 'date': date})
+#   print( 'Results Found: {}'.format( results.count()))
+#   print()
+#   for x in results:
+#     print( x[ 'name'])
+
+# findEvent( '2012/12/31')
+# findEvent( '-279')
