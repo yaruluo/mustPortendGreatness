e271b937b8e3c929661054176e054b77a9520fec
Yaru <yluo00@stuy.edu>
2020-03-09 20:14:20 +0000

    ioafjaosuj

--- 10_mongo/app.py
+++ 10_mongo/app.py
@@ -3,29 +3,30 @@
 # K#10: Import/Export Bank
 # 2020-02-29
 
-# dataset: historical events in American history
-# description: 
+# dataset: historical events in American history 
 # link: http://www.vizgr.org/historical-events/search.php?format=json&begin_date=-3000000&end_date=20151231&lang=en
 # Pro Tip: You can change begin_date and end_date in URL to get events in a specific interval. 
 
-
-from bson.json_util import loads
 from pymongo import MongoClient
+from bson.json_util import loads
+from json import JSONDecoder
 
-db.catalog.drop() 
-catalog = db.catalog
-with open( 'history.json', 'r') as file:
-  content = file.readlines()
-  for line in content:
-    catalog.insert_one( loads( line))
+dec = JSONDecoder( object_pairs_hook = lambda x: x)
+j = dict( JSONDecoder( object_pairs_hook = lambda x: x).decode( open( 'history.json', 'r').read()))
+
+client = MongoClient()
+db = client.test
+events = db.events
+for event in map( lambda x: x[-1], j[ 'result']):
+ 	bson.json_util.loads( item)
 
 def findEvent( date):
-  '''On this date...'''
-  results = catalog.find( { 'date': date})
-  print( 'Results Found: {}'.format( results.count()))
-  print()
-  for x in results:
-    print( x[ 'name'])
+	'''All events on a date.'''
+	results = events.find( {'date': date})
+	print( 'date: {}'.format( date))
+	print( 'results found: {}'.format( results.count()))
+	for x in results:
+		print( x[ 'name'])
 
 findEvent( '2012/12/31')
 findEvent( '-279')
