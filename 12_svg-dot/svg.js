e7d23b307236950af2e6dc68fcd2ef6a86b1f187
Yaru Luo <yluo00@stuy.edu>
2020-03-29 14:59:31 -0400

    connected dots

--- 12_svg-dot/svg.js
+++ 12_svg-dot/svg.js
@@ -4,25 +4,51 @@
 // K#12: Connect the Dots
 // 2020-03-30
 
-var pic = document.getElementById("vimage");
-var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-//var l = document.createElementNS("http://www.w3.org/2000/svg", "line");
+var oldX = null;
+var oldY = null;
 var firstClick = true;
 
-c.setAttribute("cx", 0);
-c.setAttribute("cy", 0);
-c.setAttribute("r", "5");
-c.setAttribute("fill", "orange");
-c.setAttribute("stroke", "black");
-
-pic.appendChild(c);
+var pic = document.getElementById("vimage");
 pic.addEventListener( "click", function( e){
+    var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+   
+    c.setAttribute("r", "5");
+    c.setAttribute("fill", "orange");
+    c.setAttribute("stroke", "black");
+    
     console.log( 'click');
     console.log( e.offsetX);
     console.log( e.offsetY);
     c.setAttribute( 'cx', e.offsetX);
     c.setAttribute( 'cy', e.offsetY);
 
-   
-});
+    pic.appendChild(c);
+
+    var l = document.createElementNS("http://www.w3.org/2000/svg", "line");
+    if( firstClick){
+	firstClick = false;
+	console.log( firstClick);
+    }
+    else{
+	l.setAttribute( 'x1', oldX);
+	l.setAttribute( 'y1', oldY);
+	l.setAttribute( 'x2', e.offsetX);
+	l.setAttribute( 'y2', e.offsetY);
+	l.setAttribute( 'stroke', 'black');
+	pic.appendChild(l);
+    }
+
+    oldX = e.offsetX;
+    oldY = e.offsetY;
     
+
+});
+
+
+// Event listener for clear button
+document.getElementById('clear').addEventListener("click", function(e) {
+    firstClick = true;
+    console.log("clear");
+    // e.preventDefault();
+    
+});
