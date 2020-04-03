768506d0e9dd8d83b2def1a3b54aa607a311d1a0
Yaru Luo <yluo00@stuy.edu>
2020-04-02 20:40:04 -0400

    observations

--- 14_svg/svg.js
+++ 14_svg/svg.js
@@ -3,75 +3,124 @@
 // K13: Ask Circles [Change || Die]
 // 2020-03-31
 
-var pic = document.getElementById('vimage');
-var clear_button = document.getElementById("clear");
-var move_button = document.getElementById("move");
+var pic = document.getElementById("vimage");
+var id = 0;
+var btn_move = document.getElementById("move");
+var btn_xtra = document.getElementById("xtra");
 
-var animating=false;
+//Function for drawing new circles
+pic.addEventListener("click", function(e){
+  if(e.target == pic){ //if mouse is on svg blank canvas
+    var c = document.createElementNS("http://www.w3.org/2000/svg", "circle"); //creating a new circle
 
-var draw = function(e) {
-  if (e.target == pic){
-    var x = e.offsetX; // mouse xcor
-    var y = e.offsetY; // mouse ycor
-    // make circle
-    var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-    c.setAttribute("cx", x);
-    c.setAttribute("cy", y);
-    c.setAttribute("r", 10);
+    c.setAttribute("r", "15");
     c.setAttribute("fill", "blue");
-    //c.setAttribute("stroke", "black");
-    var direction = Math.floor(Math.random() * 6.2832);
-    c.setAttribute("dx", 10*Math.sin(direction));
-    c.setAttribute("dy", 10*Math.cos(direction));
-    pic.appendChild(c);
-  }
-};
+    // c.setAttribute("stroke", "blue");
 
-var changeCircle = function(e) {
-  if (e.target.getAttribute("fill") == "blue"){   // if circle blue
+    c.setAttribute("cx", e.offsetX); //setting circle's position
+    c.setAttribute("cy", e.offsetY);
+
+    c.setAttribute("inc_x", 1); //setting up initial moving increments
+    c.setAttribute("inc_y", 1);
+
+    c.addEventListener("click", change); //add change color functionality
+
+    pic.appendChild(c);
+  };
+});
+
+//Function to change color from blue to cyan and removing/creating circles
+var change = function(e){
+  if(e.target != pic && e.target.getAttribute("fill") != "cyan"){ //if mouse is on a blue circle
     e.target.setAttribute("fill", "cyan");
   }
-  else { // if circle != blue
-    e.target.setAttribute("cx", Math.floor(Math.random() * 450));
-    e.target.setAttribute("cy", Math.floor(Math.random() * 450));
-    e.target.setAttribute("fill", "blue");
-  }
+  else{
+    var newX = Math.floor(Math.random() * 501);
+    var newY = Math.floor(Math.random() * 501);
+    e.target.remove(); //remove old circle
+    var c = document.createElementNS("http://www.w3.org/2000/svg", "circle"); //creating a new circle
+
+    c.setAttribute("r", "15");
+    c.setAttribute("fill", "blue");
+    // c.setAttribute("stroke", "blue");
+
+    c.setAttribute("cx", newX); //setting circle's position
+    c.setAttribute("cy", newY);
+
+    c.setAttribute("inc_x", 1); //setting up initial moving increments
+    c.setAttribute("inc_y", 1);
+
+    c.addEventListener("click", change); //add change color functionality
+
+    pic.appendChild(c);
+  };
 };
 
-var clear = function(){
-  while (pic.firstChild){
-    pic.removeChild(pic.firstChild);
-  }
-  if (id!=null){
-    window.cancelAnimationFrame(id);
-  }
-  animating = false;
-}
-
-move_button.addEventListener('click', function(e) {
-    console.log( 'here');
-    for (i = 0; i < pic.children.length; i++){
-	child = pic.children[i];
-	console.log( child);
-	child.setAttribute( 'cx', child.getAttribute( 'cx')+1);
-	child.setAttribute( 'cy', child.getAttribute( 'cy')+1);
+//Function to move circles in DVD fashion
+var move = function(e){
+  var d = document.getElementsByTagNameNS("http://www.w3.org/2000/svg", "circle"); //get all the circles
+  for (var i = 0; i < d.length; i++){
+    console.log(d[i]);
+    var dot = d[i];
+    var x = parseInt(dot.getAttribute("cx")); //retrieve and parse x and y values to int
+    var y = parseInt(dot.getAttribute("cy"));
+    var inc_x = parseInt(dot.getAttribute("inc_x")); //retrieve parse increment values to int
+    var inc_y = parseInt(dot.getAttribute("inc_y"));
+    if (x > pic.getAttribute("width") - 15){ //change increment values according to direction
+      dot.setAttribute("inc_x", -1);
     }
-
-    
-  // e.target.setAttribute("cx", parseInt(e.target.getAttribute("cx")) + parseInt(e.target.getAttribute("dx")));
-  // e.target.setAttribute("cy", parseInt(e.target.getAttribute("cy")) + parseInt(e.target.getAttribute("dy")));
-  // if (e.target.getAttribute("cx") > 490 || e.target.getAttribute("cx") < 10){
-  //   e.target.setAttribute("dx", -1 * e.target.getAttribute("dx"))
-  // }
-  // if (e.target.getAttribute("cy") > 490 || e.target.getAttribute("cy") < 10){
-  //   e.target.setAttribute("dy", -1 * e.target.getAttribute("dy"))
-  // }
+    else if (x < 15){
+      dot.setAttribute("inc_x", 1);
+    }
+    if (y > pic.getAttribute("height") - 15){
+      dot.setAttribute("inc_y", -1);
+    }
+    else if (y < 15){
+      dot.setAttribute("inc_y", 1);
+    }
+    x += inc_x; //add new increment
+    y += inc_y;
+    dot.setAttribute("cx", x); //move to new positio
+    dot.setAttribute("cy", y);
+  }
   if (id != 0){ //animation
     id = window.requestAnimationFrame(move);
   }
+};
+
+//Extra function for changing circle color
+var xtra = function(e){
+  console.log("here");
+  var d = document.getElementsByTagNameNS("http://www.w3.org/2000/svg", "circle"); //get all the circles
+  for (var i = 0; i < d.length; i++){
+    var dot = d[i];
+    console.log(dot);
+      dot.setAttribute("fill", '#FFA500'); //change their color
+  }
+};
+
+
+
+//Event listener for movement animation
+btn_move.addEventListener("click", function(e){
+    window.cancelAnimationFrame(id);
+    id = window.requestAnimationFrame(move);
+  }
+);
+
+//Event listener for xtra
+btn_xtra.addEventListener("click", xtra);
+
+//Event listener for clear button
+document.getElementById("clear").addEventListener("click", function(e){
+  console.log("clear");
+  window.cancelAnimationFrame(id); //stop animation
+  id = 0;
+
+  var fc = pic.firstChild; //remove all circles
+  while(fc) {
+    console.log("removing " + fc + "...");
+    pic.removeChild(fc);
+    fc = pic.firstChild;
+  }
 });
-
-
-pic.addEventListener('click', draw);
-clear_button.addEventListener('click', clear);
-//move_button.addEventListener('click', move);
