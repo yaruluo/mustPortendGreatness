dd785a2d1f4cfc9f36ec1c86c8b8e90ddf3ba5fb
Yaru Luo <yluo00@stuy.edu>
2020-04-16 03:30:28 -0400

    goodnight

--- 18_d3/static/js/main.js
+++ 18_d3/static/js/main.js
@@ -7,52 +7,14 @@ let created = false;
 
 let visualization = document.getElementById('visualization')
 
-let decade = 1940;
+let decade = 1950;
 
-document.getElementById('render-button').addEventListener('click', () => {
-    if (!created) {
-        let svg = createSVG();
-        render(svg);
-        created = true;
-    }
-    window.scrollTo(0, visualization.offsetTop);
-});
+let somethingElse;
 
-const createSVG = () => {
-    visualization.innerHTML += `
-        <div class="container-fluid vh-100">
-            <div class="col">
-                <div class="row py-5 justify-content-center align-items-center">
-                    <div class="h2"><b>Decade: <span id="decade">${decade}</span>s<b></div>
-                </div>
-                <div class="row justify-content-center align-items-center pb-2" id="svg-container">
-                </div>
-                <div class="row d-flex justify-content-between pt-5">
-                <div class="h3 px-5 button" id="previous"><b>&#9664; Previous Decade</b></div>
-                <div class="h3 px-5 button" id="next"><b>Next Decade &#9654;</b></div>
-                </div>
-            </div>
-        </div>`
-    return d3.select('#svg-container').append('svg')
-        .attr("viewBox", [0, 0, 975, 610])
-        .attr("width", "60%");
-};
-
-const render = async (svg) => {
-    let us = await d3.json('static/json/states-albers-10m.json');
-
-    svg.append("g")
-        .selectAll("path")
-        .data(topojson.feature(us, us.objects.states).features)
-        .join("path")
-        .attr("fill", ( d)=>{
-	    // console.log( d);
-	    return 'red';
-	})
-        .attr("d", d3.geoPath());
-    // console.log( us.objects.states);
-
+const getData = async() => {
     // dates = ARRAY ["1960", "1961", "1965", "1966", "1966", "1968", "1968",...]
+    somethingElse = await getData();
+    console.log( somethingElse);
     let data = await d3.csv( "static/csv/scrubbed.csv");
     let dates = [];
     let UFOstates = [];
@@ -98,6 +60,57 @@ const render = async (svg) => {
 	stateData[ currentDecade][ currentState] += 1;
     }
     console.log( stateData);
+
+    return stateData;
+};
+
+document.getElementById('render-button').addEventListener('click', () => {
+    if (!created) {
+        let svg = createSVG();
+        render(svg);
+        created = true;
+    }
+    window.scrollTo(0, visualization.offsetTop);
+});
+
+const createSVG = () => {
+    visualization.innerHTML += `
+        <div class="container-fluid vh-100">
+            <div class="col">
+                <div class="row py-5 justify-content-center align-items-center">
+                    <div class="h2"><b>Decade: <span id="decade">${decade}</span>s<b></div>
+                </div>
+                <div class="row justify-content-center align-items-center pb-2" id="svg-container">
+                </div>
+                <div class="row d-flex justify-content-between pt-5">
+                <div class="h3 px-5 button" id="previous"><b>&#9664; Previous Decade</b></div>
+                <div class="h3 px-5 button" id="next"><b>Next Decade &#9654;</b></div>
+                </div>
+            </div>
+        </div>`
+    return d3.select('#svg-container').append('svg')
+        .attr("viewBox", [0, 0, 975, 610])
+        .attr("width", "60%");
+};
+
+const render = async (svg) => {
+    let us = await d3.json('static/json/states-albers-10m.json');
+
+    svg.append("g")
+        .selectAll("path")
+        .data(topojson.feature(us, us.objects.states).features)
+        .join("path")
+        .attr("fill", ( d) => {
+	    console.log( d);
+	    let stateName = d[ 'properties'][ 'name'];
+	    console.log( somethingElse);
+	    console.log( String( decade));
+	    let numOfSightings = data[ String( decade)][ stateName];
+	    console.log( numOfSightings);
+	    return 'red';
+	})
+        .attr("d", d3.geoPath());
+    // console.log( us.objects.states);
 };
 
 const getThing = async( svg) => {
